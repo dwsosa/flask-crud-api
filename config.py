9470369239156391ad3d2d45f6d4d405f1e627e3
David W Sosa <sosa.david88@gmail.com>
2022-05-01 02:55:07 -0400

    added gitignore and config file for credentials

---
+++ config.py
@@ -0,0 +1,5 @@
+USERNAME="NICE"
+PASSWORD="TRY"
+DATABASE_NAME = ""
+PORT = ""
+DIALECT="" #postgres, mysql, sqlite, etc.
--- server.py
+++ server.py
@@ -1,23 +1,31 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, Response, make_response, jsonify, abort
+from config import *
 
 app = Flask(__name__)
 
+############################# DATABASE SETUP ############################
+LOCAL_DB = f"{DIALECT}://{USERNAME}/{PASSWORD}@localhost:{PORT}/{DATABASE_NAME}"
+
+# default to local db if no DB environmental variable provided i.e., not a production environment
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or LOCAL_DB
 
 ############################# API ROUTES ############################
 # list all cars for sale
-@app.route("/forsale")
+@app.route("/forsale", methods=["GET"])
 def all_cars_for_sale():
     # print("test")
     return "<p>Hello, World!</p>"
 
 # list details for specific car using VIN for reference
-@app.route("/forsale/<VIN>")
+@app.route("/forsale/<VIN>", methods=["GET"])
 def details_for_specific_car(VIN):
     # print("param value: {}".format(VIN))
     return "<p>Hello, World!</p>"
 
 # list details regarding a transaction by referencing employee ID and the orderID
-@app.route("/employee/<employeeId>/order/<orderId>")
+@app.route("/employee/<employeeId>/order/<orderId>", methods=["GET"])
 def add_ons_for_specific_car(employeeId, orderId):
     # print("employee value: {}\norder value: {}".format(employeeId, orderId))
     return "<p>Hello, World!</p>"
@@ -41,8 +49,6 @@ def resource_not_found(e):
 @app.route("/<path:path>")
 def hello_world(path):
     return "<p>{}<br><br>RESOURCE NOT FOUND <br>ERROR 404 <br>NO MATCHING ROUTES ON SERVER</p>".format(path), 404
-
-
 
 if __name__ =='__main__':
     app.run(port=718)
